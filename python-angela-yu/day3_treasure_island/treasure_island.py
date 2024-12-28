3767730284dd96ec311f9af8a76d7e2bc76c92ee
Dina Nabila <dinanabb@gmail.com>
2024-12-28 18:25:25 +0700

    add .lower() to .input()

--- python-angela-yu/day3_treasure_island/treasure_island.py
+++ python-angela-yu/day3_treasure_island/treasure_island.py
@@ -24,19 +24,19 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 print("You're at a cross road. Where do you want to go?")
-choice = input("Type 'left' or 'right': ")
+choice = input("Type 'left' or 'right': ").lower()
 
 if choice == 'right':
     print('\nYou happened to run into your mom and were immediately taken home. Game over.\n')
 elif choice == 'left':
     print("\nYou see a river. There's a crocodile, too, there, seeing directly into your eyes. Better swim, or wait him to go?")
-    choice = input("Type 'swim' or 'wait': ")
+    choice = input("Type 'swim' or 'wait': ").lower()
     if choice == 'swim':
         print('\nThe crocodile enjoy his nice dinner tonight. Game over.\n')
     elif choice == 'wait':
         print('\nThe crocodile find his female friend swimming on another side of river, and he suddenly gone from your sight. You can swim safely now.')
         print('Now you found three doors. Which one will you choose? Red, Yellow, or Blue?')
-        choice = input("Type 'red' or 'yellow' or 'blue': ")
+        choice = input("Type 'red' or 'yellow' or 'blue': ").lower()
         if choice == 'red':
             print("\nIt's an empty room, and suddenly you're locked out by someone and can't get out. Game over.\n")
         elif choice == 'blue':
