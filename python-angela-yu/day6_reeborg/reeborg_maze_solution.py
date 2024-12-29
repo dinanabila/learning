bf8cc01c73fe901d438a96ee17928a3a51f05a15
Dina Nabila <dinanabb@gmail.com>
2024-12-29 20:26:28 +0700

    add (temporary) reeborg maze solution

---
+++ python-angela-yu/day6_reeborg/reeborg_maze_solution.py
@@ -0,0 +1,30 @@
+# WARNING!!
+# INI BELUM SOLVED
+# SKIP DULU
+# BELUM BELAJAR DEBUG
+
+# link reeborg: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# def turn_right(): 
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def turn_around():
+#     turn_left()
+#     turn_left()
+
+# while at_goal() == False:
+#     if front_is_clear() and right_is_clear():
+#         if is_facing_north():
+#             move()
+#         else:
+#             turn_around()
+#             build_wall()
+#     elif front_is_clear():
+#         move()
+#     elif not front_is_clear():
+#         if not wall_on_right():
+#             turn_right()
+#         else:
+#             turn_left()
