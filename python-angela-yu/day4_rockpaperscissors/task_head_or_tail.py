c905aac7278eb2e964a54d275c4bc6ddad4b7d2b
Dina Nabila <dinanabb@gmail.com>
2024-12-29 08:15:41 +0700

    head or tail

---
+++ python-angela-yu/day4_rockpaperscissors/task_head_or_tail.py
@@ -0,0 +1,14 @@
+import random
+
+nol_atau_satu = random.randint(0, 1)
+
+if nol_atau_satu == 0:
+    print("Head")
+else:
+    print("Tail")
+
+
+# tadinya nyari di dokumentasi random, nyoba nyari tau
+# bisa ga ya kira-kira langsung cemplungin list yang isinya str "head", "tail"
+# tapi belum ketemu
+# mungkin next time
