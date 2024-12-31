9c089c635b9bc42868fa9c05b7d89a47b32009f7
Dina Nabila <dinanabb@gmail.com>
2024-12-31 09:33:40 +0700

    create step1_encryption.py

---
+++ python-angela-yu/day8_caesarchiper/step1_encryption.py
@@ -0,0 +1,14 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+tujuan = input("Ketik 'encode' untuk mengenkripsi pesan, 'decode' untuk mendekripsi pesan: ").lower
+pesan = input("\nKetik pesanmu: ").lower
+shift = int(input("\nKetik kode angka rahasiamu: "))
+
+# TODO 1: Bikin fungsi enkripsi(), buat ngambil pesan dan shift sebagai input
+
+# TODO 2: Di dalam fungsi enkripsi(), tulis perintah yang bikin huruf-huruf di teks 
+# kegeser sebanyak shift langkah, lalu print hasil enkripsinya
+
+# TODO 4: Atasi problem geser z biar bisa kegeser ke a dst
+
+# TODO 3: Call fungsi enkripsi() dan pass in input dari user.
