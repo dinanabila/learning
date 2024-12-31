38bf18451b06c375ec615d4c6d11f4fd967e8ce0
Dina Nabila <dinanabb@gmail.com>
2024-12-31 22:45:41 +0700

    add angela's solution to step2_decryption.py

--- python-angela-yu/day8_caesarchiper/step2_decryption.py
+++ python-angela-yu/day8_caesarchiper/step2_decryption.py
@@ -12,7 +12,7 @@ def enkripsi(pesan_awal, banyak_loncatan):
         else:
             hasil += alphabet[(alphabet.index(huruf) + banyak_loncatan) % len(alphabet)]
     
-    print(hasil)
+    print("hasil: ", hasil)
 
 
 # TODO 1: Bikin fungsi dekripsi(), yang inputnya pesan_awal dan banyak_loncatan
@@ -25,16 +25,42 @@ def dekripsi(pesan_awal, banyak_loncatan):
             hasil += " "
         else:
             hasil += alphabet[(alphabet.index(huruf) - banyak_loncatan) % len(alphabet)]
-    print(hasil)
+    print("hasil: ", hasil)
 
 # TODO 3: Kombinasikan fungsi enkripsi() dan dekripsi() ke dalam satu fungsi: caesar()
 # Pakai nilai dari variable 'tujuan' buat nentuin program, pakai dekripsi, atau enkripsi
-def caesar(metode):
-    if metode == "enkripsi":
-        enkripsi(pesan, shift)
-    elif metode == "dekripsi":
-        dekripsi(pesan, shift)
-    else:
-        print("Typo di tujuan. Coba lagi (ketik dekripsi / enkripsi)")
+# def caesar(metode):
+#     if metode == "enkripsi":
+#         enkripsi(pesan, shift)
+#     elif metode == "dekripsi":
+#         dekripsi(pesan, shift)
+#     else:
+#         print("Typo di tujuan. Coba lagi (ketik dekripsi / enkripsi)")
 
-caesar(tujuan)
+# caesar(tujuan)
+
+# ===================
+# NYOBA SOLUSI ANGELA
+# ===================
+
+# ternyata todo 3 yang dimaksud itu bukan yang kayak aku bikin
+# tapi bener-bener nyatuin jadi satu, biar ga repetitif
+# bener sih, biar ga berat juga programnya. 
+# oke, jadi ini versi benernya:
+
+def caesar(pesan_awal, banyak_loncatan, enkripsi_atau_dekripsi):
+    hasil =""
+    for huruf in pesan_awal:
+        if huruf == " ":
+            hasil += " "
+        else:
+            if enkripsi_atau_dekripsi == 'enkripsi':
+                hasil += alphabet[(alphabet.index(huruf) + banyak_loncatan) % len(alphabet)]
+            elif enkripsi_atau_dekripsi == 'dekripsi':
+                hasil += alphabet[(alphabet.index(huruf) - banyak_loncatan) % len(alphabet)]
+            else:
+                print("Typo di tujuan. Coba lagi (ketik dekripsi / enkripsi)")
+    
+    print("hasil:", hasil)
+
+caesar(pesan, shift, tujuan)
