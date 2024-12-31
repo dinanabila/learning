41ff541876cb1799aa87d55113938efd5672402d
Dina Nabila <dinanabb@gmail.com>
2024-12-31 19:38:23 +0700

    update step2_decryption.py: coba solve sendiri dulu

--- python-angela-yu/day8_caesarchiper/step2_decryption.py
+++ python-angela-yu/day8_caesarchiper/step2_decryption.py
@@ -1,6 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# tujuan = input("Ketik 'encode' untuk mengenkripsi pesan, 'decode' untuk mendekripsi pesan: ").lower()
+tujuan = input("Ketik 'enkripsi' untuk mengenkripsi pesan, 'dekripsi' untuk mendekripsi pesan: ").lower()
 pesan = input("\nKetik pesanmu: ").lower()
 shift = int(input("\nKetik kode angka rahasiamu: "))
 
@@ -14,12 +14,27 @@ def enkripsi(pesan_awal, banyak_loncatan):
     
     print(hasil)
 
-enkripsi(pesan, shift)
-
 
 # TODO 1: Bikin fungsi dekripsi(), yang inputnya pesan_awal dan banyak_loncatan
+def dekripsi(pesan_awal, banyak_loncatan):
 
 # TODO 2: Susun perintah yang bikin huruf-huruf di dalam pesan_awal mundur sebanyak banyak_loncatan
+    hasil = ""
+    for huruf in pesan_awal:
+        if huruf == " ":
+            hasil += " "
+        else:
+            hasil += alphabet[(alphabet.index(huruf) - banyak_loncatan) % len(alphabet)]
+    print(hasil)
 
 # TODO 3: Kombinasikan fungsi enkripsi() dan dekripsi() ke dalam satu fungsi: caesar()
-# Pakai nilai dari variable 'direction' buat nentuin program, pakai dekripsi, atau enkripsi
+# Pakai nilai dari variable 'tujuan' buat nentuin program, pakai dekripsi, atau enkripsi
+def caesar(metode):
+    if metode == "enkripsi":
+        enkripsi(pesan, shift)
+    elif metode == "dekripsi":
+        dekripsi(pesan, shift)
+    else:
+        print("Typo di tujuan. Coba lagi (ketik dekripsi / enkripsi)")
+
+caesar(tujuan)
