e076b7c2974f00c1d03f7a2d07eebd6cff8e2f30
Dina Nabila <dinanabb@gmail.com>
2025-01-02 19:35:31 +0700

    update blackjack.py

--- python-angela-yu/day011_blackjack/blackjack.py
+++ python-angela-yu/day011_blackjack/blackjack.py
@@ -6,5 +6,5 @@
 # 3. j/q/k dianggap 10
 # 4. as dianggap 11
 # 5. deck: cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-# 6. peluang keambil sama untuk setiap kartu
+# 6. peluang terambilnya setiap kartu adalah sama
 # 7. dealer: komputer
