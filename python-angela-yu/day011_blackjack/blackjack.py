319204b8474b4e3cbeadaf7a2ac1a883f9dd3271
Dina Nabila <dinanabb@gmail.com>
2025-01-02 21:02:22 +0700

    update art.py

--- python-angela-yu/day011_blackjack/blackjack.py
+++ python-angela-yu/day011_blackjack/blackjack.py
@@ -8,3 +8,6 @@
 # 5. deck: cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 # 6. peluang terambilnya setiap kartu adalah sama
 # 7. dealer: komputer
+
+from art import blackjack
+print(blackjack)
