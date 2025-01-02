ef58ec385d1d1b0654d8e1208531a7b8ee6be0f9
Dina Nabila <dinanabb@gmail.com>
2025-01-02 19:28:56 +0700

    add house rules

--- python-angela-yu/day011_blackjack/blackjack.py
+++ python-angela-yu/day011_blackjack/blackjack.py
@@ -1,1 +1,10 @@
 # program blackjack
+
+# aturan house:
+# 1. deck unlimited
+# 2. ga ada joker
+# 3. j/q/k dianggap 10
+# 4. as dianggap 11
+# 5. deck: cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# 6. peluang keambil sama untuk setiap kartu
+# 7. dealer: komputer
