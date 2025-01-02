90e0ef823d87c947ce43fd09749f9fd6bbaa0265
Dina Nabila <dinanabb@gmail.com>
2025-01-02 13:41:42 +0700

    add angela's solution

--- python-angela-yu/day9_secret_auction/secret_auction.py
+++ python-angela-yu/day9_secret_auction/secret_auction.py
@@ -21,32 +21,50 @@ print("\nSELAMAT DATANG DI SECRET AUCTION\n")
 def secret_auction(auction):
     max_bid = 0
     pemenang = ""
+    # ==========
+    # VERSI ACUU
+    # ==========
+    # for i in auction:
+    #     if auction[i] > max_bid:
+    #         max_bid = auction[i]
+    
+    # for key, value in auction.items():
+    #     if value  == max_bid:
+    #         pemenang += " " + key
+    # ==============
+    # END VERSI ACUU
+    # ==============
+
+    # ============
+    # VERSI ANGELA
+    # ============
     for i in auction:
         if auction[i] > max_bid:
             max_bid = auction[i]
-    
+    pemenang += " " + i
+    # ================
+    # END VERSI ANGELA
+    # ================
+    print("\nList semua bid:")
     for key, value in auction.items():
-        if value  == max_bid:
-            pemenang += " " + key
-    print(f"Pemenangnya:{pemenang}")
+        print(f"{key}: Rp{value}")
+    print("\n")
+    print(f"Pemenangnya{pemenang} dengan bid Rp{value}")
 
 auction = {}
-
 orang_selanjutnya = "ya"
+lanjut_bidding = True
 
-while orang_selanjutnya == "ya":
+while lanjut_bidding:
     nama = input("Nama: ")
-    bid = int(input("Jumlah bid: "))
+    bid = int(input("Jumlah bid: Rp"))
     orang_selanjutnya = input("Masih ada yang mau nge-bid? (ketik 'ya' kalau masih): ").lower()
 
     auction[nama] = bid
 
-    print("\n" * 100)
-
     if orang_selanjutnya != "ya":
-        print("\nList semua bid:")
-        for key, value in auction.items():
-            print(f"{key}: {value}")
-        print("\n")
+        lanjut_bidding = False
         secret_auction(auction)
+    elif orang_selanjutnya == 'ya':
+        print("\n" * 100)
     
