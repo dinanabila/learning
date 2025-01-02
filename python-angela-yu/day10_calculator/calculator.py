40143972857445dd464ff27500c45cb92735d2e7
Dina Nabila <dinanabb@gmail.com>
2025-01-02 17:42:09 +0700

    update calculator.py

--- python-angela-yu/day10_calculator/calculator.py
+++ python-angela-yu/day10_calculator/calculator.py
@@ -1,7 +1,7 @@
+from art import kalkulator
+
 def jumlah(n1, n2):
     return n1 + n2
-
-# TODO: Bikin fungsi kurang, kali, bagi
 
 def kurang(n1, n2):
     return n1 - n2
@@ -22,5 +22,27 @@ operasi = {
     "/": bagi,
 }
 
-# TODO: Cobain operasinya dari dictionary operasi
-print(operasi["+"](2, 4))
+# # TODO: Cobain operasinya dari dictionary operasi
+# print(operasi["+"](2, 4))
+kalkulator_hidup = True
+while kalkulator_hidup:
+    print(kalkulator)
+    hasil = 0
+    lanjut = True
+
+    a = float(input("Angka pertama: "))
+    while lanjut:
+        print("+\n-\n*\n/")
+        hitung = input("Operasi: ")
+        b = float(input("Angka kedua: "))
+        a = operasi[hitung](a, b)
+        print(f"hasil: {a}")
+        lanjut_hitungan = input("Ketik 'y' kalau mau lanjutin hitungan ini. Ketik 'n' kalau mau ngitung hitungan baru: ").lower()
+        
+        if lanjut_hitungan == 'y':
+            lanjut = True
+        elif lanjut_hitungan == 'n':
+            lanjut = False
+    
+    if lanjut_hitungan == 'n':
+        print("\n" * 100)
