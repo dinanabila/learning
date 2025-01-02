3fdc58f64b8e47c1e2b232b892cff2ac4f208dc0
Dina Nabila <dinanabb@gmail.com>
2025-01-03 03:20:47 +0700

    update draft.py

--- python-angela-yu/day011_blackjack/draft.py
+++ python-angela-yu/day011_blackjack/draft.py
@@ -14,22 +14,74 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 hand = []
+kartu_pemain = []
+kartu_komputer = []
 
-def first_hand():
-    for kartu in range(3):
-        kartu = random.choice(cards)
-        hand.append(kartu)
+def ambil_kartu(banyak_kartu, kartu_siapa):
+    hand = kartu_siapa
+    for _ in range(banyak_kartu):
+        hand.append(random.choice(cards))
     return hand
+
+def hitung_skor(kartu_siapa):
+    skor = 0
+    for i in range(len(kartu_siapa)):
+        skor += kartu_siapa[i]
+    return skor
 
 # main_lagi = input("Hai, aku komputer. \nMau main satu ronde blackjack sama aku? \nKetik 'y' kalau mau, 'g' kalau ga: ").lower()
 # print(blackjack)
-# aksi = input("Ketik 'y' kalau mau tambah kartu, ketik 'g' kalau mau pass aja: ").lower()
 
+# TODO 1: bikin first card pemain dan komputer pakai random
+kartu_pemain = ambil_kartu(2, kartu_pemain)
+kartu_komputer = ambil_kartu(2, kartu_komputer)
+skor_pemain = hitung_skor(kartu_pemain)
+skor_komputer = hitung_skor(kartu_komputer)
+print(f"    Kartu di tangan kamu: {kartu_pemain}, skor sekarang: {skor_pemain}")
+print(f"    Kartu pertama komputer: {kartu_komputer[0]}")
 
-# TODO: bikin first card pemain dan komputer pakai random
-kartu_perdana_pemain = first_hand()
-kartu_perdana_komputer = first_hand()
+aksi_pemain = input("Ketik 'y' kalau mau tambah kartu, ketik 'g' kalau mau pass aja: ").lower()
 
-print(f"komputer first hand: {kartu_perdana_komputer}")
-print(f"first hand pemain: {kartu_perdana_pemain}")
+# TODO 3: bikin list buat pilihan random komputer: pass / ambil kartu lagi
+pilihan_aksi = ['pass', 'hand']
 
+# TODO 2: susun kondisional if else mentah biar ada gambaran
+if aksi_pemain == 'y':
+    kartu_pemain = ambil_kartu(1, kartu_pemain)
+    skor_pemain = hitung_skor(kartu_pemain)
+    print(f"    Kartu di tangan kamu: {kartu_pemain}, skor sekarang: {skor_pemain}")
+    print(f"    Kartu pertama komputer: {kartu_komputer[0]}")
+    if skor_pemain > 21:
+        print("Skormu kebablasan. Kamu kalah :(")
+    else:
+        aksi_komputer = random.choice(pilihan_aksi)
+        if aksi_komputer == 'pass':
+            if skor_pemain > skor_komputer:
+                print("Kamu menang :D")
+            elif skor_pemain == skor_komputer:
+                print("Kamu kalah")
+            else:
+                print("Seri")
+        elif aksi_komputer == 'hand': 
+            kartu_komputer = ambil_kartu(1, kartu_komputer)
+            skor_komputer = hitung_skor(kartu_komputer)
+            if kartu_komputer > 21:
+                print("Skor lawan kebablasan. Kamu menang :D")
+            # else:
+                # balik lagi ke atas
+elif aksi_pemain == 'n':
+    aksi_komputer = random.choice(pilihan_aksi)
+    if aksi_komputer == 'pass':
+        if skor_pemain > skor_komputer:
+            print("Kamu menang :D")
+        elif skor_pemain == skor_komputer:
+            print("Kamu kalah")
+        else:
+            print("Seri")
+    elif aksi_komputer == 'hand': 
+        kartu_komputer = ambil_kartu(1, kartu_komputer)
+        skor_komputer = hitung_skor(kartu_komputer)
+        if kartu_komputer > 21:
+            print("Skor lawan kebablasan. Kamu menang :D")
+        # else:
+            # balik lagi ke atas
