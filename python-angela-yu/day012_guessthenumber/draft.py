459d5ac11dc523fbc63f0a80eba5d0b4d86986a6
Dina Nabila <dinanabb@gmail.com>
2025-01-04 16:25:07 +0700

    update draft.py: todo 1 - 3

---
+++ python-angela-yu/day012_guessthenumber/draft.py
@@ -0,0 +1,10 @@
+import random
+
+# TODO 1: bikin input yang diperlukan
+tebak_angka = input("Ketik angka tebakanmu: ")
+# eh, ini doang keknya o_o
+
+# TODO 2: bikin angka random 1 - 100, masukin ke variabel
+yang_benar = random.randint(1, 100)
+
+# TODO 3: print selamat datang
