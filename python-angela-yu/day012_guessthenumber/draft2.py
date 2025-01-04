265ca502846e1ecd73c63a5584bb5d03f5b3c8e2
Dina Nabila <dinanabb@gmail.com>
2025-01-04 19:46:29 +0700

    add draft2.py

---
+++ python-angela-yu/day012_guessthenumber/draft2.py
@@ -0,0 +1,12 @@
+# melihat draft 1 yang semrawut
+# maka
+# timbul keinginan untuk
+# menghilangkan kebiasaan nulis bertele-tele
+
+# tolong ya, 
+# wahai aku, 
+# biasakan manfaatin fitur fungsi, 
+# juga, biasakan sederhanakan alurnya
+# biar ga jorok
+
+
