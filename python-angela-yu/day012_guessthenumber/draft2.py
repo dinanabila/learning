618b20dffde40aa3227ff466c1323d63c30dd2f9
Dina Nabila <dinanabb@gmail.com>
2025-01-05 13:21:49 +0700

    update draft2.py

--- python-angela-yu/day012_guessthenumber/draft2.py
+++ python-angela-yu/day012_guessthenumber/draft2.py
@@ -16,4 +16,35 @@
 
 # let's try build them
 
+import random
+from art import logo
 
+# GLOBAL VARIABLE
+GAMPANG = 10
+SUSAH = 5
+
+# MESIN: input mode, output nyawa
+def genset_nyawa(mode_apa):
+    if mode_apa == 'a':
+        return GAMPANG
+    else:
+        return SUSAH
+
+# MESIN: input nyawa dan tebakan, output keputusan
+# def tebakan()
+
+
+# ALUR KESELURUHAN
+# START: opening
+print(logo)
+print("\nHaii, ayo main tebak-tebakan angka!\nDari angka 1 - 100, coba tebak, angka apa yang kupikirkan?")
+yang_benar = random.randint(1, 100)
+print(f"ini contekan yang bener buat debugging: {yang_benar}")
+print("Tapi sebelum itu, biar lebih menantang, kita bakal batasi jumlah tebakan yang bisa kamu tebak.")
+print("\nKamu mau mode gampang (boleh 10 kali nebak), atau susah (cuma bisa 5 kali nebak)? ")
+
+# INPUT MODE
+mode = input("\nKetik 'a' kalau mau gampang, 'b' kalau mau susah: ").lower()
+
+nyawa = genset_nyawa(mode)
+print(nyawa)
