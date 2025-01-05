799c37b18f055a648d26dc565904e5b79d0ce0c2
Dina Nabila <dinanabb@gmail.com>
2025-01-05 15:17:41 +0700

    add tebak_angka.py and update draft2.py

--- python-angela-yu/day012_guessthenumber/tebak_angka.py
+++ python-angela-yu/day012_guessthenumber/tebak_angka.py
@@ -1,43 +1,18 @@
-# melihat draft 1 yang semrawut
-# maka
-# timbul keinginan untuk
-# menghilangkan kebiasaan nulis bertele-tele
-
-# tolong ya, 
-# wahai aku, 
-# biasakan manfaatin fitur fungsi, 
-# juga, biasakan sederhanakan alurnya
-# biar ga jorok
-
-# ==============
-
-# di flowchart 2
-# aku punya beberapa mesin a.k.a fungsi
-
-# let's try build them
-
 import random
 from art import logo
 
-# GLOBAL VARIABLE
+# VARIABEL GLOBAL
 GAMPANG = 10
 SUSAH = 5
 
+# FUNGSI
 def game_tebak_angka():
-    # [MESIN] input: mode, output: nyawa
     def genset_nyawa(mode_apa):
         if mode_apa == 'a':
             return GAMPANG
         else:
             return SUSAH
 
-    # [MESIN] input: nyawa dan tebakan, output: tebakan
-    # def nebak(): 
-    #     return int(input("\nKetik angka tebakanmu: "))
-    # mesin ini masukin ke dalam loop di mesin tebakan, 
-    # di variabel tebakan_user
-
-    # [MESIN] input: none, output: start / end
     def lagi_atau_ga():
         yuk = input("\nMasih mau main tebak angka sama aku? Ketik 'y' kalau iya, 'g' kalau ngga: ").lower()
         if yuk == 'g':
@@ -45,7 +20,6 @@ def game_tebak_angka():
         else:
             return game_tebak_angka()
 
-    # [MESIN] input: nyawa dan tebakan, output: hasil
     def hasil(jumlah_nyawa, tebakan_user, yang_benar):
         if jumlah_nyawa == 1 and tebakan_user != yang_benar:
             print("Yah, sisa kesempatanmu udah habis.")
@@ -53,7 +27,6 @@ def game_tebak_angka():
             print("Selamatt, tebakanmu benar! :D")
         return lagi_atau_ga()
         
-    # [MESIN] input: nyawa dan tebakan, output: keputusan
     def tebakan(jumlah_nyawa, tebakan_user, yang_benar):
         while tebakan_user != yang_benar:
             tebakan_user = int(input("\nKetik angka tebakanmu: "))
@@ -67,24 +40,17 @@ def game_tebak_angka():
                 print(f"\n******* TERSISA {jumlah_nyawa} KESEMPATAN LAGI *******")
             else:
                 return hasil(jumlah_nyawa, tebakan_user, yang_benar)
+            
     angka = random.randint(1, 100)
-    print(f"ini contekan yang bener buat debug: {angka}")
-
     mode = input("\nKetik 'a' kalau mau gampang, 'b' kalau mau susah: ").lower()
     nyawa = genset_nyawa(mode)
     angka_tebakan = None
     tebakan(nyawa, angka_tebakan, angka)
 
-# ALUR KESELURUHAN
-# START: opening
+# PROGRAM 
 print(logo)
 print("\nHaii, ayo main tebak-tebakan angka!\nDari angka 1 - 100, coba tebak, angka apa yang kupikirkan?")
 print("Tapi sebelum itu, biar lebih menantang, kita bakal batasi jumlah tebakan yang bisa kamu tebak.")
 print("\nKamu mau mode gampang (boleh 10 kali nebak), atau susah (cuma bisa 5 kali nebak)? ")
 
 game_tebak_angka()
-
-
-
-
-
