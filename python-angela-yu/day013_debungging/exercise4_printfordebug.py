c31df686b94a67931b2ab889c146b5a3a19456de
Dina Nabila <dinanabb@gmail.com>
2025-01-06 11:40:45 +0700

    add exercise4.py

---
+++ python-angela-yu/day013_debungging/exercise4_printfordebug.py
@@ -0,0 +1,5 @@
+jumlah_kata_per_hal = 0
+jumlah_hal = int(input("Jumlah halaman: "))
+jumlah_kata_per_hal == int(input("Jumlah kata per halaman: "))
+total_kata = jumlah_hal * jumlah_kata_per_hal
+print(total_kata)
