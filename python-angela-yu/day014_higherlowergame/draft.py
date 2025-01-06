a330721ef6bfae48bb408cb3b02a841e898fa9e6
Dina Nabila <dinanabb@gmail.com>
2025-01-06 14:31:22 +0700

    update draft.py

--- python-angela-yu/day014_higherlowergame/draft.py
+++ python-angela-yu/day014_higherlowergame/draft.py
@@ -1,6 +1,8 @@
 # bikin input jawaban user
+input("Siapa yang followernya lebih banyak? Ketik 'A' atau 'B': ").lower()
 
 # ambil acak 2 data dari game_data pakai random choice
+# jangan sampai data yang diambil sama. harus beda, kan mau dibandingin
 
 # bikin fungsi buat ngecek perbandingan jumlah follower kedua data itu
 # outputnya jawaban yang benar (yang punya lebih banyak follower)
