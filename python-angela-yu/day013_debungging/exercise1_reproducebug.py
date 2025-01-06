795e5c03876b07f4454dc3cddf248f12586d7abd
Dina Nabila <dinanabb@gmail.com>
2025-01-06 13:54:18 +0700

    rename directory

--- python-angela-yu/day013_debungging/exercise1_reproducebug.py
+++
@@ -1,9 +0,0 @@
-# belajar reproducing bug
-
-# tujuannya: buat ngelabelin bug, dan membasminya
-
-from random import randint
-
-dice_images = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
-dice_num = randint(1, 6) # --> ini harusnya 0, 5. kan index list mulainya dari 0
-print(dice_images[dice_num])
