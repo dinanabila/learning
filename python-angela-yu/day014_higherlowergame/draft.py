7942e27ba665044ab14488e68e175bb867c99473
Dina Nabila <dinanabb@gmail.com>
2025-01-06 14:26:18 +0700

    update draft.py: add steps

---
+++ python-angela-yu/day014_higherlowergame/draft.py
@@ -0,0 +1,11 @@
+# bikin input jawaban user
+
+# ambil acak 2 data dari game_data pakai random choice
+
+# bikin fungsi buat ngecek perbandingan jumlah follower kedua data itu
+# outputnya jawaban yang benar (yang punya lebih banyak follower)
+
+# bikin fungsi buat ngecek jawaban user bener atau ga
+
+    # bikin kondisional, kalau bener, print current score + lanjut ke soal baru, 
+    # kalau salah, print total skor akhir dan hentikan program
