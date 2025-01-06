6e702fedea223ee31936b5095402c0c06be29e42
Dina Nabila <dinanabb@gmail.com>
2025-01-06 11:45:27 +0700

    update exercise4.py

--- python-angela-yu/day013_debungging/exercise4_printfordebug.py
+++ python-angela-yu/day013_debungging/exercise4_printfordebug.py
@@ -3,3 +3,11 @@ jumlah_hal = int(input("Jumlah halaman: "))
 jumlah_kata_per_hal == int(input("Jumlah kata per halaman: "))
 total_kata = jumlah_hal * jumlah_kata_per_hal
 print(total_kata)
+
+# debug
+print("\n====debug====")
+print(f"jumlah hal: {jumlah_hal}")
+print(f"jumlah kata per hal: {jumlah_kata_per_hal}") # setelah diprint, ternyata bug nya ini
+
+# kenapa tetep 0, si jumlah_kata_per_hal?
+# soalnya pakai ==, bukan =
