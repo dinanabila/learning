74d48754e17cc5045d2fcd2c7c7f01f7ac126cd3
Dina Nabila <dinanabb@gmail.com>
2025-01-07 14:12:35 +0700

    add exercise1_turtle.py

---
+++ python-angela-yu/day016_coffeemachine_oop/exercise1_turtle.py
@@ -0,0 +1,17 @@
+# belajar oop
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+# print(timmy)
+timmy.shape("turtle")
+
+# ubah warna timmy
+timmy.color("bisque4")
+
+# ubah posisi timmy
+timmy.forward(100)
+
+my_screen = Screen()
+# print(my_screen.canvheight)
+my_screen.exitonclick()
