b5b54e05ced701b26903d936d84d6209aecc8902
Dina Nabila <103731788+dinanabila@users.noreply.github.com>
2025-02-11 08:26:37 +0700

    Delete directory

--- web-development-dicoding/learning-js/TryCatchError/try-catch-finally.js
+++
@@ -1,8 +0,0 @@
-try {
-    console.log("Awal blok try");
-    console.log("Akhir blok try");
-} catch (error) {
-    console.log("Baris ini diabaikan");
-} finally {
-    console.log("Akan tetap dieksekusi");
-}
--- web-development-dicoding/learning-js/TryCatchError/try-catch.js
+++
@@ -1,9 +0,0 @@
-try {
-    console.log("Awal blok try");   // (1)
-    errorCode;                      // (2)
-    console.log("Akhir blok try");  // (3)
-} catch (error) {
-    console.log(error.name);  // (4)
-    console.log(error.message);
-    console.log(error.stack);
-}
