2d74510fcae29b1946a8227155514b79e0730042
Dina Nabila <103731788+dinanabila@users.noreply.github.com>
2025-07-09 08:29:39 +0700

    Update 5.sql
    add DATE_FORMAT to Shift_date

--- sql/sqlzoo/help-desk/easy/5.sql
+++ sql/sqlzoo/help-desk/easy/5.sql
@@ -1,3 +1,3 @@
-SELECT Shift_date, Shift_type, first_name, last_name 
+SELECT DATE_FORMAT(Shift_date, '%Y-%m-%d') AS Shift_date, Shift_type, first_name, last_name 
 FROM Shift, Staff
 WHERE Shift.Manager = Staff.Staff_code
